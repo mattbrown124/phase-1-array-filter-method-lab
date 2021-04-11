9a705964b8224a67e8f388f86e8f3565e60d54d1
Matt Brown <matthewtbrown124@gmail.com>
2021-04-11 12:33:55 -0500

    commit tree 76787f5de3faf03a4a73e54b13fa9826c8736168

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.slice(0, string.length) === string;
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.name === string;
+    });
+} 
